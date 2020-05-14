8bcda1872944ff3a1e1eef49532266de70d3dfdc
Estevan Maito <ejmaito@gmail.com>
2020-05-14 10:47:56 -0300

    feat(saybye): add say bye
    It adds the option to say good bye
    
    fix #3

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 module.exports = {
   sayHi: 'Hi',
+  sayBye: 'Bye',
   sing: 'Welcome to the jungle',
 }
--- test/index.test.js
+++ test/index.test.js
@@ -11,6 +11,12 @@ describe('mylib says', () => {
     })
   })
 
+  describe('sayBye', () => {
+    it('should be a string', () => {
+      expect(typeof myLib.sayBye).toBe('string')
+    })
+  })
+
   describe('sing', () => {
     it('should sing a song', () => {
       expect(myLib.sing).toBe('Welcome to the jungle')
